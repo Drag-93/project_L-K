660036fb7ea9a74fe062c865ab187ae15ddb3e13
gusdn000615 <gusWkdwjddml@gmail.com>
2026-02-04 16:43:14 +0900

    merge수정3

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,27 +2,33 @@ import { useEffect, useRef, useState } from "react";
 import { Link } from 'react-router-dom'
 
 
-const Main = () => {
+import ReactFullpage from '@fullpage/react-fullpage';
 
-  return (
-    <div className="outer">
-      <section className='section sec1'>
-        <div className="main_wrap">
-
-          <h1>
-          진단에서 치료까지<br/>
-          피부과 전문의가 직접 치료합니다
-          </h1>
-        </div>
-      </section>
-      <section className='section sec2'>
-        2
-      </section>
-      <section className='section sec3'>
-        3
-      </section>
-    </div>
-  )
-}
+const Main = () => (
+  <ReactFullpage
+    scrollingSpeed={1000}
+    navigation // 우측 점 내비게이션 활성화
+    render={({ state, fullpageApi }) => {
+      return (
+        <ReactFullpage.Wrapper>
+          <div className="section sec1">
+            <div className="main_wrap">
+              <h1>
+                <span className="reveal_text line1">진단에서 치료까지</span>
+                <span className="reveal_text line2">피부과 전문의가 직접 치료합니다</span>
+              </h1>
+            </div>
+          </div>
+          <div className="section sec2">
+            <h1>지점 소개</h1>
+          </div>
+          <div className="section sec3">
+            <h1>예약 안내</h1>
+          </div>
+        </ReactFullpage.Wrapper>
+      );
+    }}
+  />
+);
 
 export default Main
