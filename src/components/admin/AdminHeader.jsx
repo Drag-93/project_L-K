fe9b5c10f681e956bde1f5530d2a4714635fd874
Drag <faithyg93@gmail.com>
2026-02-04 17:44:23 +0900

    admin_role_yg

--- src/components/admin/AdminHeader.jsx
+++ src/components/admin/AdminHeader.jsx
@@ -1,10 +1,26 @@
-import React from "react";
-
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { logoutF } from "../../store/slice/inputSlice";
+import { useNavigate } from "react-router-dom";
 const AdminHeader = () => {
+  const user = useSelector((state) => state.input.user);
+
+  const dispatch = useDispatch();
+
+  const logoutFn = () => {
+    dispatch(logoutF());
+    alert("로그아웃 되었습니다");
+  };
+
   return (
     <div className="adminHeader">
       <div className="adminHeader-con">
-        <h1>Header</h1>
+        <ul>
+          <li>
+            <h1>{user?.userName}관리자님</h1>
+          </li>
+          <button onClick={logoutFn}>로그아웃</button>
+        </ul>
       </div>
     </div>
   );
