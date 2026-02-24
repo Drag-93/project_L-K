c3711c6d2cb880d397dee67f21185d23353129d1
gusdn000615 <gusWkdwjddml@gmail.com>
2026-02-24 17:46:55 +0900

    merge수정

--- src/layout/AuthLayout.jsx
+++ src/layout/AuthLayout.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import Header from "../components/common/Header";
-import { NavLink, Outlet } from "react-router-dom";
+import { NavLink, Outlet, useParams } from "react-router-dom";
 import Footer from "../components/common/Footer";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 const AuthLayout = () => {
+  const dispatch = useDispatch();
   const state = useSelector((state) => state);
+  const isState = useSelector((state) => state.input.isState);
+  const param = useParams();
+  console.log(param);
   return (
     <>
       <Header />
@@ -13,10 +17,10 @@ const AuthLayout = () => {
         <div className="auth_list_wrap">
           <div className="auth_aside_wrap">
             <ul>
-              <li><NavLink to={`/auth/mypage/${state.input.user.id}`}>내정보</NavLink></li>
-              <li><NavLink to={`/auth/mypayment/${state.input.user.id}`}>결재내역</NavLink></li>
-              <li><NavLink to={`/auth/myqna/${state.input.user.id}`}>내 Q&A</NavLink></li>
-              <li><NavLink to={`/auth/myreview/${state.input.user.id}`}>내 리뷰</NavLink></li>
+              <li><NavLink to={`/auth/mypage/${state.input.user?.id}`}>내정보</NavLink></li>
+              <li><NavLink to={`/auth/mypayment/${state.input.user?.id}`}>결재내역</NavLink></li>
+              <li><NavLink to={`/auth/myqna/${state.input.user?.id}`}>내 Q&A</NavLink></li>
+              <li><NavLink to={`/auth/myreview/${state.input.user?.id}`}>내 리뷰</NavLink></li>
             </ul>
           </div>
           <Outlet />
